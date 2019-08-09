3f60c78c7c875965043bbd984dacd9993e350149
ElenaPavlova <hell618lena@gmail.com>
2019-08-09 17:15:25 +0500

    Added annotation @Autowired and @Scheduled

--- src/main/java/com/company/app/controller/MyController.java
+++ src/main/java/com/company/app/controller/MyController.java
@@ -2,23 +2,24 @@ package com.company.app.controller;
 
 import com.company.app.beans.MyBean;
 import com.company.app.service.ScheduledTask;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
-import java.util.Timer;
 
 @Controller
 public class MyController {
+    @Autowired
     private ScheduledTask task;
+    @Autowired
     private MyBean bean;
 
-    public MyController(ScheduledTask task, MyBean bean) {
+/*    public MyController(ScheduledTask task, MyBean bean) {
         this.task = task;
         this.bean = bean;
-    }
+    }*/
 
     @GetMapping("/")
     public ModelAndView printHome(ModelMap model) {
--- src/main/java/com/company/app/service/ScheduledTask.java
+++ src/main/java/com/company/app/service/ScheduledTask.java
@@ -1,9 +1,9 @@
 package com.company.app.service;
 
 import com.company.app.beans.MyBean;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.Timer;
 import java.util.TimerTask;
 
 @Service
@@ -11,11 +11,7 @@ public class ScheduledTask extends TimerTask {
     private MyBean myBean;
 
     public ScheduledTask(MyBean myBean) {
-
         this.myBean = myBean;
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(this, 0, 1000);
-
     }
 
     public void changeText(String text) {
@@ -26,6 +22,7 @@ public class ScheduledTask extends TimerTask {
         return myBean.getText();
     }
 
+    @Scheduled(fixedDelay = 1000)
     public void run() {
         System.out.println(myBean.getText());
     }
